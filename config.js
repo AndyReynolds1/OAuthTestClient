8eeaf922bcf00f2388773c5ab3e6407f6bccde37
stal <the.stally@gmail.com>
2021-12-03 20:09:45 +0000

    Add changes for Docker version

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration
 
 module.exports = {
-  port: 8000,
-  authorization_endpoint: "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize",
-  token_endpoint: "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/token",
-  userinfo_endpoint: "https://graph.microsoft.com/oidc/userinfo",
-  device_authorization_endpoint: "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/devicecode",
-  userSearch_endpoint: "https://graph.microsoft.com/v1.0/users",
-  clientId: "",
-  clientSecret: "",
-  code_verifier: "someValue",
-  userId: ""
+  port: process.env.PORT || 8000,
+  authorization_endpoint: process.env.AUTHZ_ENDPOINT || "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize",
+  token_endpoint: process.env.TOKEN_ENDPOINT || "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/token",
+  userinfo_endpoint: process.env.USERINFO_ENDPOINT || "https://graph.microsoft.com/oidc/userinfo",
+  device_authorization_endpoint: process.env.DEVICE_AUTHZ_ENDPOINT || "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/devicecode",
+  userSearch_endpoint: process.env.USER_SEARCH_ENDPOINT || "https://graph.microsoft.com/v1.0/users",
+  clientId: process.env.CLIENT_ID || "",
+  clientSecret: process.env.CLIENT_SECRET || "",
+  code_verifier: process.env.CODE_VERIFIER || "someValue",
+  userId: process.env.USERID || ""
 };
